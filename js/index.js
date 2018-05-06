d310b9d8c7e42746be53468da6bf7c98247151b8
Kelly Villalobos <kvdesign1978@gmail.com>
2018-05-05 22:28:00 -0500

    added email to contact page

--- js/index.js
+++ js/index.js
@@ -10,3 +10,10 @@ $(document).ready(function () {
 
 });
 
+$( '.js-input' ).keyup(function() {
+    if( $(this).val() ) {
+        $(this).addClass('not-empty');
+    } else {
+        $(this).removeClass('not-empty');
+    }
+});
